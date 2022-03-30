594689824854204acfe8a8fcb214d3ac4da81adf
zack pathounourack <zackpathou@gmail.com>
2022-03-30 15:40:50 +0200

    first commit

---
+++ src/components/Countries.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "./Card";
+
+const Countries = () => {
+  // la variable data passe par setData qui poura etre modifié
+  const [data, setData] = useState([]);
+  const [rangeValue, setRangeValue] = useState(36);
+  const [selectedRadio, setSelectedRadio] = useState("");
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
+
+// le useffect c'est quand le composant est monté Le useEffect se joue lorsque le composant est monté
+  useEffect(() => {
+// axios permet de faire des méthodes fetchs ex un .get,post... qui retourne une promesse ex .then en gros c une requete HTTP qu on envoie
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((res) => setData(res.data));
+  }, []);
+
+  return (
+    <div className="countries">
+      <ul className="radio-container">
+        <input
+          type="range"
+          min="1"
+          max="250"
+          defaultValue={rangeValue}
+          onChange={(e) => setRangeValue(e.target.value)}
+        />
+        {radios.map((continent) => (
+          <li>
+            <input
+              type="radio"
+              id={continent}
+              name="continentRadio"
+              checked={continent === selectedRadio}
+              onChange={(e) => setSelectedRadio(e.target.id)}
+            />
+            <label htmlFor={continent}>{continent}</label>
+          </li>
+        ))}
+      </ul>
+      {selectedRadio && (
+        <button onClick={() => setSelectedRadio("")}>
+          Annuler la recherche
+        </button>
+      )}
+      <ul>
+        {data
+          .filter((country) => country.continents[0].includes(selectedRadio))
+          .sort((a, b) => b.population - a.population)
+          .slice(0, rangeValue)
+          .map((country, index) => (
+            <Card key={index} country={country} />
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Countries;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./styles/index.scss";
 
+//On appel le composant App.js avec l'id root dans le dossier public index.html
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App />   
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
